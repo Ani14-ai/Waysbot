3f50d2fe9bc032b18f5f0b7daecce958533f4ad7
Ani14-ai <138982309+Ani14-ai@users.noreply.github.com>
2024-06-20 16:31:59 +0530

    Update rp.py

--- rp.py
+++ rp.py
@@ -26,7 +26,7 @@ db_connection_string = (
     "Server=103.145.51.250;"
     "Database=PMO360_DB;"
     "UID=PMOlogbook_Usr;"
-    "PWD=PMO_log360!x4;"
+    "PWD=G9x^0n0t4;"
 )
 def preprocess_text(text):
     tokens = word_tokenize(text.lower())
